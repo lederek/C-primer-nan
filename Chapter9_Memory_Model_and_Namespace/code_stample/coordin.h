2352b6bfd1cc8f6686facdb0e68da6112db4f25c
Chennc <guhu8798bjf@gmail.com>
2022-03-06 17:31:45 +0800

    [Chapter 9 内存模型和名称空间]

---
+++ Chapter9_Memory_Model_and_Namespace/code_stample/coordin.h
@@ -0,0 +1,14 @@
+// coordin.h -- struct templates and function prototypes
+// structure templates
+#ifndef COORDIN_H_
+#define COORDIN_H_
+
+struct polar
+{
+    double distance;    // distance from origin
+    double angle;       // direction from origin
+};
+
+
+
+#endif
