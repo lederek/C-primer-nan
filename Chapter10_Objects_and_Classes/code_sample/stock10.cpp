c1ce4c9c6617012ee1a4946addde40329990cbd0
Chennc <guhu8798bjf@gmail.com>
2022-03-10 23:43:38 +0800

    [Chapt10 对象和类】 1. 实现类成员函数 2. 声明和定义构造函数 3. 使用构造函数 4. 默认构造函数 5. 析构函数

---
+++ Chapter10_Objects_and_Classes/code_sample/stock10.cpp
@@ -0,0 +1,9 @@
+// stock10.cpp -- Stock class with constructor
+#include<iostream>
+#include"stock10.h"
+
+// constructors (verbose version)
+Stock::Stock()      // default constr
+{
+    std::cout << "Defalut"
+}
